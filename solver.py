383ffa57bbcc11d32bfea355121afc634a25f392
Ramsey2022 <abovethelotus@yahoo.com>
2023-11-23 10:02:06 -0800

    created function to check for duplicates throughout board and added docstrings

--- solver.py
+++ solver.py
@@ -12,6 +12,9 @@ board = [
 ]
 
 def print_board(grid):
+    """
+    Prints board in sudoku format
+    """
     for i in range(len(grid)):
         if i % 3 == 0 and i != 0:
             print('- - - - - - - - - - - -')
@@ -26,7 +29,34 @@ def print_board(grid):
                     print(str(grid[i][j]) + " ", end="")
 
 def find_empty(grid):
+    """
+    Checks for empty spaces
+    """
     for i, row in enumerate(grid):
         for j, val in enumerate(row):
             if val == 0:
                 return(i,j)
+
+def valid(grid, num, pos):
+    """
+    Checks rows and columns for duplicates
+    """
+    #loop through row
+    for i in range(len(grid[0])):
+        if grid[pos[0]][i] == num and pos[1] != i:
+            return False
+    
+    #loop through column
+    for i in range(len(grid)):
+        if grid[i][pos[1]] == num and pos[0] != i:
+            return False
+        
+    #checking 3x3 boxes
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if grid[i][j] == num and (i, j) != pos:
+                return False
+    return True
